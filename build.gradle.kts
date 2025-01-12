551987820e7a283f5fe0bcd764b9840de152ed97
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2025-01-12 19:02:20 -0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.kafka:kafka-clients:3.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("org.apache.kafka:kafka-streams:3.6.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("redis.clients:jedis:5.0.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
